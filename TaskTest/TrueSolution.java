5af1b2db3851d7e6cffeead92fcb13d46a1f5152
Nik0190 <nik0190@mail.ru>
2022-09-23 18:41:48 +0300

    Create TrueSolution.java

---
+++ TaskTest/TrueSolution.java
@@ -0,0 +1,21 @@
+import javax.xml.crypto.Data;
+import java.io.*;
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.sql.SQLOutput;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+
+public class TrueSolution {
+    public static void main(String[] args) throws IOException {
+
+    }
+}
